be59830aeab95e6df21484a8df0f0625d77696ec
sunwuquan <15064777789@163.com>
2018-04-27 17:33:01 +0800

    js完成部分1.完成进度条:获取分数来分配百分比;2.点赞改变颜色,并且获取点赞数进行加减

--- js/evaluation.js
+++ js/evaluation.js
@@ -1,1 +1,31 @@
+$(".likes").on("click","img",function () {
+    if($(this).attr("src")=="./images/evaluation/expression/baton-2.png"){
+        $(this).attr("src","./images/evaluation/expression/baton-1.png");
+        var text=$(this).parents(".likes").find("span").text();
+        text++;
+        $(this).parents(".likes").find("span").text(text);
+        console.log(text);
 
+    }else {
+        $(this).attr("src","./images/evaluation/expression/baton-2.png");
+        var text=$(this).parents(".likes").find("span").text();
+        if(text==0){
+            text=0;
+        }else {
+            text--;
+        }
+        $(this).parents(".likes").find("span").text(text);
+    }
+});
+var top_=$(".top-font_right").text();
+$(".top-font_right").css("color","#f32613");
+top_*=10;
+$(".top_bar_active").width(top_+"%");
+var center_=$(".center_font_right").text();
+$(".center_font_right").css("color","#fdc631");
+center_*=10;
+$("center_bar_active").width(center_+"%");
+var botton_=$(".bottom_font_right").text();
+$(".bottom_font_right").css("color","#ff7d04");
+botton_*=10;
+$("bottom_bar_active").width(botton_+"%");
